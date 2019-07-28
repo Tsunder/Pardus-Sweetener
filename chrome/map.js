1884ada7920da4c65977d74be9d9b45b0c7a2657
Tsunder <Tsunder@users.noreply.github.com>
2019-07-28 10:56:55 -0400

    Update map.js

--- chrome/map.js
+++ chrome/map.js
@@ -491,9 +491,9 @@ SectorMap.prototype = {
 			speed -= parseInt( moveField[0].textContent );
 			speed += this.VISC[ currentTileType ];
 			
-			if ( ( this.Navigation == 1 && currentTileType == 'o' )
-				|| ( this.Navigation == 2 && currentTileType == 'g' )
-			    || ( this.Navigation == 3 && currentTileType == 'e' ) ) {
+			if ( ( this.Navigation > 0 && currentTileType == 'o' )
+				|| ( this.Navigation > 1 && currentTileType == 'g' )
+			    || ( this.Navigation > 2 && currentTileType == 'e' ) ) {
 					speed += 1;
 				}
 			return speed;
